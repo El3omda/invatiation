6301ff4fc0bc0e411dc01b0e47cbc33e03730506
El3omda <el3om0a@gmail.com>
2023-05-31 15:53:38 +0200

    Fix

--- resources/views/invites/show.blade.php
+++ resources/views/invites/show.blade.php
@@ -185,14 +185,20 @@
             {{-- Invitation Description --}}
             <h4 class="fw-bold dir">
 
+                @if (!preg_match('/[^A-Za-z0-9]/', $invite->description))
+                    {{-- <sup><sup><i class="fa-sharp fa-solid fa-quote-left" style="color: rgba(250,44,99,1);"></i></sup></sup> --}}
+                    en
+                @else
+                    <sub><sub><i class="fa-sharp fa-solid fa-quote-left" style="color: rgba(250,44,99,1);"></i></sub></sub>
+                @endif
+
                 {{ $invite->description }}
 
                 @if (!preg_match('/[^A-Za-z0-9]/', $invite->description))
                     {{-- <sup><sup><i class="fa-sharp fa-solid fa-quote-left" style="color: rgba(250,44,99,1);"></i></sup></sup> --}}
                     en
                 @else
-                    {{-- <sub><sub><i class="fa-sharp fa-solid fa-quote-right" style="color: rgba(250,44,99,1);"></i></sub></sub> --}}
-                    Ar
+                    <sub><sub><i class="fa-sharp fa-solid fa-quote-right" style="color: rgba(250,44,99,1);"></i></sub></sub>
                 @endif
             </h4>
 
