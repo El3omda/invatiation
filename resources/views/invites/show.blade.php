79e548fef5ae9c93be19f6b985c943759c777126
El3omda <el3om0a@gmail.com>
2023-05-31 15:44:53 +0200

    Fix

--- resources/views/invites/show.blade.php
+++ resources/views/invites/show.blade.php
@@ -191,7 +191,7 @@
                     <sub><sub><i class="fa-sharp fa-solid fa-quote-right" style="color: rgba(250,44,99,1);"></i></sub></sub>
                 @endif
                 {{ $invite->description }}
-                @if (App::getLocale() == 'en')
+                @if (!preg_match('/[^A-Za-z0-9]/', $invite->description))
                     <sub><sub><i class="fa-sharp fa-solid fa-quote-right" style="color: rgba(250,44,99,1);"></i></sub></sub>
                 @else
                     <sup><sup><i class="fa-sharp fa-solid fa-quote-left" style="color: rgba(250,44,99,1);"></i></sup></sup>
